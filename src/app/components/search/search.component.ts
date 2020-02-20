5f1e78e5c070d6db8859af63401881c3af03a520
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-20 10:11:31 -0500

    [+] CS > Http - Spotify: Implementa operador pipe/map al Observador

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -11,8 +11,10 @@ export class SearchComponent {
 
   constructor( public _spotifyService: SpotifyService ) {
       this ._spotifyService .getArtists()       // Obtiene el Observable del Service
-          .subscribe( response => {             // Se subscribirse a el para obtener la data
-              console .log( 'Search', response );
+          .subscribe( response => {             // Se subscribirse a el para obtener la data (Se recomienda solo usar en el subscribe lógica específica del componente)
+              console .group( 'Search' );
+              console .log( 'Artistas', response );
+              console .groupEnd();
           });
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 /** Opciones para Reescribir la Cabecera */
 const
@@ -27,7 +28,16 @@ export class SpotifyService {
         let url = 'https://api.spotify.com/v1/search?query=Metallica&type=artist&limit=20';
 
         /** Hace la Peticion HTTP */
-        return this ._httpClient .get( url, httpOptions );            // Retorna Observable de petición GET: Al URL con reescritura de cabeceras
+        return this ._httpClient
+            .get( url, httpOptions )    // Retorna Observable de petición GET: Al URL con reescritura de cabeceras
+            .pipe(                      // RXJS: pipe operador que permite combinar múltiples funciones que se pasan comop parametro. Retorna una nueva funcion que ejecuta las funciones compuestas en secuencia
+                // RXJS: map operator funciona exactamente igual para Observables que para arrays.
+                map( ( response : any ) => {        // Logica adicional para recorrer la data específica de estructura del Objeto JSON que deseamos (Opcional)
+                    this .artists = response .artists .items;
+                    return this .artists;
+                })
+            );
+
     }
 
 }
